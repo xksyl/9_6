62d9f4c71c41d251cf1cfab92c3a9fea55887efd
xksyl <egorka2004212@gmail.com>
2024-09-27 14:27:53 +0500

    Add files via upload

---
+++ module_9_6.py
@@ -0,0 +1,11 @@
+def all_variants(text):
+    for i in range(1, len(text) + 1):
+        for j in range(len(text) - i + 1):
+            yield text[j:j + i]
+
+
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
